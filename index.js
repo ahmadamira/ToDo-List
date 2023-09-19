a76cc6dc013073fcffbfbd71e595ce0a110f96d9
ahmad amira <ahmad_amira@outlook.com>
2023-09-20 01:06:08 +0300

    added session storage

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
-let taskId = 1;
-async function fetchTasks() {
+let taskId = parseInt(sessionStorage.getItem("taskId")) || 1;
+let taskListArray = JSON.parse(sessionStorage.getItem("taskListArray")) || [];
+
+function loadTasksFromSessionStorage() {
+  const taskList = document.getElementById("taskList");
+  taskList.innerHTML = "";
+
+  taskListArray.forEach((task) => {
+    const newRow = taskList.insertRow();
+    newRow.innerHTML = `
+        <td>${task.id}</td>
+        <td>${task.description}</td>
+        <td>${task.userId}</td>
+        <td>${task.status}</td>
+        <td class="actions">
+            <button class="done-button" onclick="markDone(this)">Done</button>
+            <button class="delete-button" onclick="deleteTask(this)">Delete</button>
+        </td>
+    `;
+    taskId = Math.max(taskId, task.id + 1);
+  });
+}
+
+window.onload = () => {
+  loadTasksFromSessionStorage();
+};
+
+async function fetchTasksFromAPI() {
   try {
     const response = await fetch("https://dummyjson.com/todos");
     const tasks = await response.json();
+
     console.log(tasks);
 
     const taskList = document.getElementById("taskList");
@@ -10,24 +37,44 @@ async function fetchTasks() {
     tasks.todos.forEach((task) => {
       const newRow = taskList.insertRow();
       newRow.innerHTML = `
-                <td>${taskId}</td>
-                <td>${task.todo}</td>
-                <td>${task.userId}</td>
-                <td>Pending</td>
-                <td class="actions">
+            <td>${taskId}</td>
+            <td>${task.todo}</td>
+            <td>${task.userId}</td>
+            <td>Pending</td>
+            <td class="actions">
                 <button class="done-button" onclick="markDone(this)">Done</button>
                 <button class="delete-button" onclick="deleteTask(this)">Delete</button>
-                </td>
-            `;
+            </td>
+        `;
+
+      const newTask = {
+        id: taskId,
+        description: task.todo,
+        userId: task.userId,
+        status: "Pending",
+      };
+
+      taskListArray.push(newTask);
 
       taskId++;
     });
+
+    sessionStorage.setItem("taskId", taskId);
+    sessionStorage.setItem("taskListArray", JSON.stringify(taskListArray));
   } catch (error) {
     console.error("Error fetching tasks:", error);
   }
 }
 
-fetchTasks();
+function loadTasks() {
+  if (taskListArray.length === 0) {
+    fetchTasksFromAPI();
+  } else {
+    loadTasksFromSessionStorage();
+  }
+}
+
+loadTasks();
 
 function addTask() {
   const taskInput = document.getElementById("taskInput");
@@ -35,18 +82,30 @@ function addTask() {
 
   if (todoDescription !== "") {
     const taskList = document.getElementById("taskList");
-
     const newRow = taskList.insertRow();
+
+    const task = {
+      id: taskId,
+      description: todoDescription,
+      userId: 1,
+      status: "Pending",
+    };
+
+    taskListArray.push(task);
+
+    sessionStorage.setItem("taskId", taskId);
+    sessionStorage.setItem("taskListArray", JSON.stringify(taskListArray));
+
     newRow.innerHTML = `
-            <td>${taskId}</td>
-            <td>${todoDescription}</td>
-            <td>1</td>
-            <td>Pending</td>
-            <td class="actions">
-                <button onclick="markDone(this)">Done</button>
-                <button onclick="deleteTask(this)">Delete</button>
-            </td>
-        `;
+        <td>${taskId}</td>
+        <td>${todoDescription}</td>
+        <td>1</td>
+        <td>Pending</td>
+        <td class="actions">
+            <button class="done-button" onclick="markDone(this)">Done</button>
+            <button class="delete-button" onclick="deleteTask(this)">Delete</button>
+        </td>
+      `;
 
     taskId++;
     taskInput.value = "";
@@ -56,12 +115,29 @@ function addTask() {
 function markDone(button) {
   const row = button.closest("tr");
   row.cells[3].textContent = "Done";
+
+  const taskId = parseInt(row.cells[0].textContent);
+  const task = taskListArray.find((task) => task.id === taskId);
+  if (task) {
+    task.status = "Done";
+
+    sessionStorage.setItem("taskListArray", JSON.stringify(taskListArray));
+  }
 }
 
 function deleteTask(button) {
   const row = button.closest("tr");
   row.remove();
+
+  const taskId = parseInt(row.cells[0].textContent);
+  const taskIndex = taskListArray.findIndex((task) => task.id === taskId);
+  if (taskIndex !== -1) {
+    taskListArray.splice(taskIndex, 1);
+
+    sessionStorage.setItem("taskListArray", JSON.stringify(taskListArray));
+  }
 }
+
 function searchTask() {
   const searchInput = document.getElementById("searchInput");
   const searchText = searchInput.value.toLowerCase().trim();
